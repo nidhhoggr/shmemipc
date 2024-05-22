fcdca2b5bd17a5d142edbbb0a2e5d0c74afa27a4
joe-at-startupmedia <joseph.persie@startupmedia.com>
2024-05-22 04:11:43 +0000

    only log 0-remainders of 10,000

--- shmemipc_test.go
+++ shmemipc_test.go
@@ -28,6 +28,12 @@ func Test(t *testing.T) {
 	time.Sleep(time.Second * 1)
 }
 
+func log(i int, info string) {
+	if i%10000 == 0 || i == TEST_COUNT-1 {
+		fmt.Println(info)
+	}
+}
+
 func serverRoutine(server *ShmProvider, client *ShmProvider) {
 
 	for i := 0; i < TEST_COUNT; i++ {
@@ -35,13 +41,13 @@ func serverRoutine(server *ShmProvider, client *ShmProvider) {
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("[server] [%d] Read from client: %s\n", i, string(msg))
+		log(i, fmt.Sprintf("[server] [%d] Read from client: %s\n", i, string(msg)))
 		clientMessage := "Hello, client!"
 		err = client.Write([]byte(clientMessage))
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("[server] [%d] Write to client: %s\n", i, clientMessage)
+		log(i, fmt.Sprintf("[server] [%d] Write to client: %s\n", i, clientMessage))
 	}
 }
 
@@ -50,7 +56,7 @@ func clientRoutine(server *ShmProvider, client *ShmProvider) {
 	serverMessage := "Hello, server!"
 
 	for i := 0; i < TEST_COUNT; i++ {
-		fmt.Printf("[client] [%d] Write to server: %s\n", i, serverMessage)
+		log(i, fmt.Sprintf("[client] [%d] Write to server: %s\n", i, serverMessage))
 		err := server.Write([]byte(serverMessage))
 		if err != nil {
 			panic(err)
@@ -59,6 +65,6 @@ func clientRoutine(server *ShmProvider, client *ShmProvider) {
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("[client] [%d] Response from server: %s\n", i, string(msg))
+		log(i, fmt.Sprintf("[client] [%d] Response from server: %s\n", i, string(msg)))
 	}
 }
