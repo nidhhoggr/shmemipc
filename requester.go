08d20591c38f710f70490e38f3fb39b143b660f0
joe-at-startupmedia <joseph.persie@startupmedia.com>
2024-05-24 02:30:59 +0000

    removing network interface

--- requester.go
+++ requester.go
@@ -1,31 +1,16 @@
 package shmemipc
 
-import (
-	"bytes"
-	"fmt"
-	"io"
-)
-
-type IpcRequester struct {
-	BidirectionalShmem
-	conn *IpcRequesterConn
-}
+type IpcRequester BidirectionalShmem
 
 func NewRequester(filename string) *IpcRequester {
 	responder, errResp := StartClient(filename + "_resp")
 	requester, errRqst := StartClient(filename + "_rqst")
 
 	ir := IpcRequester{
-		BidirectionalShmem: BidirectionalShmem{
-			shmResp: responder,
-			errResp: errResp,
-			shmRqst: requester,
-			errRqst: errRqst,
-		},
-	}
-
-	ir.conn = &IpcRequesterConn{
-		requester: &ir,
+		shmResp: responder,
+		errResp: errResp,
+		shmRqst: requester,
+		errRqst: errRqst,
 	}
 
 	return &ir
@@ -40,40 +25,9 @@ func (ir *IpcRequester) Write(b []byte) error {
 }
 
 func (ir *IpcRequester) GetError() error {
-	return ir.BidirectionalShmem.GetError()
+	return (*BidirectionalShmem)(ir).GetError()
 }
 
 func (ir *IpcRequester) Close() error {
-	return ir.BidirectionalShmem.Close()
-}
-
-type IpcRequesterConn struct {
-	requester *IpcRequester
-}
-
-func (ir *IpcRequester) GetConn() *IpcRequesterConn {
-	return ir.conn
-}
-
-func (ir *IpcRequesterConn) Read(b []byte) (int, error) {
-	msg, err := ir.requester.Read()
-	if err != nil {
-		return 0, err
-	}
-	fmt.Printf("[requester_conn] read msg: %s %s\n", string(msg), b)
-	src := bytes.NewBuffer(msg)
-	dst := bytes.NewBuffer(b)
-	_, err = io.Copy(dst, src)
-	fmt.Printf("[requester_conn] read src: %s, dest: %s, msg: %s\n", src, dst, string(msg))
-	return 0, err
-}
-
-func (ir *IpcRequesterConn) Write(b []byte) (int, error) {
-	fmt.Printf("[requester_conn] write msg: %s\n", string(b))
-	err := ir.requester.Write(b)
-	return 0, err
-}
-
-func (ir *IpcRequesterConn) Close() error {
-	return ir.requester.Close()
+	return (*BidirectionalShmem)(ir).Close()
 }
--- responder.go
+++ responder.go
@@ -1,32 +1,16 @@
 package shmemipc
 
-import (
-	"bytes"
-	"fmt"
-	"io"
-	"net"
-)
-
-type IpcResponder struct {
-	BidirectionalShmem
-	conn *IpcResponderConn
-}
+type IpcResponder BidirectionalShmem
 
 func NewResponder(filename string, len uint64) *IpcResponder {
 	responder, errResp := StartServer(filename+"_resp", len)
 	requester, errRqst := StartServer(filename+"_rqst", len)
 
 	ir := IpcResponder{
-		BidirectionalShmem: BidirectionalShmem{
-			shmResp: responder,
-			errResp: errResp,
-			shmRqst: requester,
-			errRqst: errRqst,
-		},
-	}
-
-	ir.conn = &IpcResponderConn{
-		responder: &ir,
+		shmResp: responder,
+		errResp: errResp,
+		shmRqst: requester,
+		errRqst: errRqst,
 	}
 
 	return &ir
@@ -41,56 +25,9 @@ func (ir *IpcResponder) Write(b []byte) error {
 }
 
 func (ir *IpcResponder) GetError() error {
-	return ir.BidirectionalShmem.GetError()
+	return (*BidirectionalShmem)(ir).GetError()
 }
 
 func (ir *IpcResponder) Close() error {
-	return ir.BidirectionalShmem.Close()
-}
-
-func (ir *IpcResponder) Accept() (net.Conn, error) {
-	panic("unimplemented")
-}
-
-func (ir *IpcResponder) Network() string {
-	return "shmemipc"
-}
-
-func (ir *IpcResponder) String() string {
-	return ir.shmResp.name
-}
-
-func (ir *IpcResponder) Addr() net.Addr {
-	return ir
-}
-
-type IpcResponderConn struct {
-	responder *IpcResponder
-}
-
-func (ir *IpcResponder) GetConn() *IpcResponderConn {
-	return ir.conn
-}
-
-func (ir *IpcResponderConn) Read(b []byte) (int, error) {
-	msg, err := ir.responder.Read()
-	if err != nil {
-		return 0, err
-	}
-	fmt.Printf("[responder_conn] read msg: %s %s\n", b, string(msg))
-	src := bytes.NewBuffer(msg)
-	dst := bytes.NewBuffer(b)
-	_, err = io.Copy(dst, src)
-	fmt.Printf("[responder_conn] read src: %s, dest: %s, msg: %s\n", src, dst, msg)
-	return 0, err
-}
-
-func (ir *IpcResponderConn) Write(b []byte) (int, error) {
-	fmt.Printf("[responder_conn] write msg: %s\n", string(b))
-	err := ir.responder.Write(b)
-	return 0, err
-}
-
-func (ir *IpcResponderConn) Close() error {
-	return ir.responder.Close()
+	return (*BidirectionalShmem)(ir).Close()
 }
